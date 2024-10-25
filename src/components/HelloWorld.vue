ddb21e89060879d6d29cefaf6d412833dc5b3db7
Giacomo Cattani <giacomo.cattani04@gmail.com>
2024-10-25 14:28:50 +0200

    FrontEnd Render

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+const data = ref(null)
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/your-endpoint')
+    data.value = response.data
+  } catch (error) {
+    console.error('There was a problem with the axios operation:', error)
+  }
+})
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p v-if="data">
+    API Data: {{ data }}
+  </p>
+
+  <p>
+  </p>
+</template>
